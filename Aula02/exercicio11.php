d11278208fee8e26ff0f17afca7f9a4fe5770ac9
Pedro Sachetto De Moraes <pedro.sachetto13@gmail.com>
2026-06-19 15:45:05 -0300

    Add files via upload

---
+++ Aula02/exercicio11.php
@@ -0,0 +1,9 @@
+<?php
+
+    $num1 = 6;
+    $num2 = 7;
+
+    echo "Multiplicação: " . $num1 * $num2;
+
+
+?>
---
+++ Aula02/exercicio13.php
@@ -0,0 +1,11 @@
+<?php
+
+$num = -5;
+
+if ($num >=0 ) {
+    echo "Numero  $num é  positivo: ";
+} else{
+    echo "Numero $num é negativo:  ";
+}
+
+?>
